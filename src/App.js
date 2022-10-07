59a5d401511356ae26b20c54f91d3963dee7a622
Robyn Garlington <robyn.garlington@hotmail.co.uk>
2022-10-07 16:32:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Heading from './components/Heading';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App-header'>
+      <Heading />
+      <p>Graduate with a masters and undergraduate in psychology and criminology turned fullstack software developer</p>
     </div>
   );
 }
---
+++ src/components/Heading.js
@@ -0,0 +1,8 @@
+const Heading = () => {
+    return(
+        <>
+        <h1>Robyn Garlington - Fullstack Software Developer</h1>
+        </>
+    )
+}
+export default Heading;
